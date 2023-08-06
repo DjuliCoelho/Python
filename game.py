246792b4bcc090774f43e379d704285a9b3f9083
Djuli Coelho <126629987+DjuliCoelho@users.noreply.github.com>
2023-08-06 20:19:45 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,21 +1,21 @@
 import random
 
-opcoes = {1: 'Pedra', 2: 'Papel', 3: 'Tesoura'}
+opcoes = {1: 'pedra', 2: 'papel', 3: 'tesoura'}
 ganhou = 'Você ganhou, uhuu'
-perdeu = 'Você perdeu, seu noob'
+perdeu = 'Você perdeu, você é muito ruim!'
 
 while True:
-    sua_escolha = input('Pedra, papel ou tesoura?')
+    sua_escolha = input('pedra, papel ou tesoura?')
     num = random.randint(1,3)
     escolha_pc = opcoes[num]
 
-    if sua_escolha == 'Pedra' and escolha_pc == 'Tesoura':
+    if sua_escolha == 'pedra' and escolha_pc == 'tesoura':
         result = ganhou
 
     elif sua_escolha == 'tesoura' and escolha_pc == 'papel':
         result = ganhou
 
-    elif sua_escolha == 'Papel' and escolha_pc == 'Pedra':
+    elif sua_escolha == 'papel' and escolha_pc == 'pedra':
         result = ganhou
 
     elif sua_escolha == escolha_pc:
@@ -27,5 +27,5 @@ while True:
     print(f'{result}, você escolheu {sua_escolha} e o pc escolheu {escolha_pc}')
     continua = input('Deseja jogar novamente? (sim ou não)')
 
-    if continua != 'Sim':
+    if continua != 'sim':
         break
